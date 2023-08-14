79f7e96f9f3a0172ec1d549cc3a8061c42305bac
AnduriCaser <tibetogunc@gmail.com>
2023-08-14 12:03:01 +0300

    index added

--- L01/index.php
+++ L01/index.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Syndore Web Challenge 1</title>
+</head>
+<body>
+    <!--
+        Mustafa there is an uuid in my secret folder -> L3NlY3JldC9zZWNyZXQudHh0
+        You understand that. Just please keep safe it for me. 
+        Thank you brother !
+    -->
+</body>
+</html>
+
+
 <?php
 require_once("./handler.php");
 if (isset($_GET["variable"])) {
